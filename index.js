8a96b79523b5b9b04d4cbace4a0e7d78fff60c50
Leonardo Contreras <LContreras@student.neumont.edu>
2021-08-17 13:33:22 -0600

    Bank Complete

---
+++ index.js
@@ -0,0 +1,8 @@
+let bankCash = 10000;
+let cashOnHand = 0;
+
+sessionStorage.setItem('bankCash', bankCash);
+sessionStorage.setItem('cashOnHand', cashOnHand);
+
+console.log(bankCash);
+console.log(cashOnHand);
